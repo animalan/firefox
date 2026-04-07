6a0eaeabcbb250c49cb0c81c4e570c3a34fa2f70
Nicolas Chevobbe <nchevobbe@mozilla.com>
2026-04-07 08:10:23 +0000

    Bug 2029168 - [devtools] Add pref for the CSS explainers work. r=devtools-reviewers,ochameau.
    Differential Revision: https://phabricator.services.mozilla.com/D291975

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2998,6 +2998,8 @@ pref("devtools.inspector.simple-highlighters-reduced-motion", false);
 // Wheter or not Enter on inplace editor in the Rules view moves focus and activates
 // next inplace editor.
 pref("devtools.inspector.rule-view.focusNextOnEnter", true);
+// Enable Rules view CSS explainers
+pref("devtools.inspector.css-explainers", false);
 
 // Whether or not the box model panel is opened in the layout view
 pref("devtools.layout.boxmodel.opened", true);
