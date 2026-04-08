d27e5e919813f60b4aa38967ee92f8d50baee662
Cosmin Sabou <csabou@mozilla.com>
2026-04-08 19:11:44 +0300

    Revert "Bug 2030019 - Remove unused inclusion of ostream, fstream and sstream r=webrtc-reviewers,profiler-reviewers,emilio,layout-reviewers,bwc,canaltinova,dshin" for causing build bustages on FuzzingInterface.
    This reverts commit 6ecd953ac6d0e22ee47feebd2bc47701f27ae368.
    
    Revert "Bug 2030019 - Fix include linter in presence of std namespace r=emilio"
    
    This reverts commit f29a4219419ae550bc251bd10f37cbc319efcf43.

--- dom/quota/test/gtest/TestUsageInfo.cpp
+++ dom/quota/test/gtest/TestUsageInfo.cpp
@@ -3,6 +3,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include <cstdint>
+#include <ostream>
 
 #include "gtest/gtest.h"
 #include "mozilla/Maybe.h"
--- js/src/vm/JSContext.cpp
+++ js/src/vm/JSContext.cpp
@@ -17,6 +17,7 @@
 #include <string.h>
 #ifdef ANDROID
 #  include <android/log.h>
+#  include <fstream>
 #endif  // ANDROID
 #ifdef XP_WIN
 #  include <processthreadsapi.h>
