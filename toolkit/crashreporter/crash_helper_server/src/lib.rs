0437b345a77c030808782358409e4a94091dae6b
Gabriele Svelto <gsvelto@mozilla.com>
2026-04-30 06:48:35 +0000

    Bug 1955963 - Move some of the crash helper platform-dependent code into its own files r=afranchuk
    Differential Revision: https://phabricator.services.mozilla.com/D289752

--- toolkit/crashreporter/crash_helper_server/src/lib.rs
+++ toolkit/crashreporter/crash_helper_server/src/lib.rs
@@ -10,9 +10,8 @@ mod crash_generation;
 mod ipc_server;
 mod logging;
 mod phc;
+mod platform;
 
-#[cfg(target_os = "android")]
-use crash_helper_common::RawIPCConnector;
 use crash_helper_common::{BreakpadData, BreakpadRawData, IPCConnector, IPCListener, Pid};
 use std::{
     ffi::{c_char, CStr, OsString},
@@ -51,7 +50,7 @@ pub unsafe extern "C" fn crash_generator_logic_desktop(
     #[cfg(any(target_os = "ios", target_os = "macos"))]
     const BOOTSTRAP_UNKNOWN_SERVICE: std::ffi::c_int = 1102;
 
-    daemonize();
+    platform::daemonize();
     logging::init();
 
     let breakpad_data = BreakpadData::new(breakpad_data);
@@ -116,7 +115,7 @@ pub unsafe extern "C" fn crash_generator_logic_android(
     pid: Pid,
     breakpad_data: BreakpadRawData,
     minidump_path: *const c_char,
-    pipe: RawIPCConnector,
+    pipe: crash_helper_common::RawIPCConnector,
 ) {
     logging::init();
 
@@ -157,47 +156,6 @@ fn main_loop(mut ipc_server: IPCServer) -> i32 {
                 return -1;
             }
             _ => {} // Go on
-        }
-    }
-}
-
-// Daemonize the current process by forking it and then immediately returning
-// in the parent. This should have been done via a double fork() in the
-// crash_helper_client crate, however the first fork() call causes issues to
-// Thunderbird on macOS 10.15 (see bug 1977514). This is a known problem with
-// macOS 10.15 implemenetation, not a flaw in our logic, and the only way to
-// work around it is to use posix_spawn() instead, which forces use to move
-// the step to reparent the crash helper to PID 1 here.
-//
-// Note that if this fails for some reason, the crash helper will still launch,
-// but not as a daemon. Not ideal but still better to have a fallback.
-#[cfg(not(target_os = "android"))]
-fn daemonize() {
-    #[cfg(not(target_os = "windows"))]
-    {
-        use nix::unistd::{fork, setsid, ForkResult};
-
-        // Create a new process group and a new session, this guarantees
-        // that the crash helper process will be disconnected from the
-        // signals of Firefox main process' controlling terminal. Killing
-        // Firefox via the terminal shouldn't kill the crash helper which
-        // has its own lifecycle management.
-        //
-        // We don't check for errors as there's nothing we can do to
-        // handle one in this context.
-        let _ = setsid();
-
-        let res = unsafe { fork() };
-        let Ok(res) = res else {
-            return;
-        };
-
-        match res {
-            ForkResult::Child => {}
-            ForkResult::Parent { child: _ } => unsafe {
-                // We're done, exit cleanly
-                nix::libc::_exit(0);
-            },
         }
     }
 }
