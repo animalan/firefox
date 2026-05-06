4a371b6fa36af74d3bbb7478ca58323cc1c93ab1
agoloman <agoloman@mozilla.com>
2026-05-06 05:45:57 +0300

    Revert "Bug 2036329, Bug 2036780, Bug 2034984, Bug 2034983. jxl decoder flushes pixels when nothing has changed. r=saschanaz" for causing rust failures.
    This reverts commit b7bef6c638dc98a55cec8919205c4a3b37848dc4.
    
    Revert "Bug 2036780. Update to small api change in jxl-rs. r=saschanaz"
    
    This reverts commit 86ba74e2926bacfcefb76393182ccfd87b93eda7.
    
    Revert "Bug 2036780. Upgrade jxl-rs to just past 0.4.3. r=saschanaz,supply-chain-reviewers,afranchuk"
    
    This reverts commit c0bffe15d774421be41f39b20214cc5f991af25d.
    
    Revert "Bug 2034984. In the jxl decoder make sure to apply color management before premultiplying alpha. r=saschanaz"
    
    This reverts commit e4cd22188a2ebfaa74e31fe0d759fb149bbb7f6b.
    
    Revert "Bug 2034984. Add per-format pixel conversion and CMS wiring in C++ jxl decoder. r=saschanaz"
    
    This reverts commit c47523165998c2ce4d0863cf874ace37e6b197e2.
    
    Revert "Bug 2034984. Add per-pixel-format detection and infrastructure to jxl decoder. r=saschanaz"
    
    This reverts commit 68bae1f3853c6788ebd047cbc60fd01836180a35.
    
    Revert "Bug 2034984. Remove internal CMS adapter from jxl-rs; simplify jxl_decoder_new API. r=saschanaz"
    
    This reverts commit b5a600b1195739f9fb23e7146708cd81ede6a8e3.
    
    Revert "Bug 2034983. Give qcms a f16 transform interface. r=jrmuizel"
    
    This reverts commit 4fc19f9fea00b5496d47caf0dc2d3d8d7ef75245.

--- image/decoders/nsJXLDecoder.h
+++ image/decoders/nsJXLDecoder.h
@@ -23,12 +23,6 @@ class nsJXLDecoder final : public Decoder {
 
   DecoderType GetType() const override { return DecoderType::JXL; }
 
-#ifdef DEBUG
-  // Debug-only (for tests): number of times WritePixelRowsToPipe has run on
-  // this decoder. Counts both partial flushes and the final frame write.
-  uint32_t GetWritePixelRowsCount() const { return mWritePixelRowsCount; }
-#endif
-
  protected:
   nsresult InitInternal() override;
   LexerResult DoDecode(SourceBufferIterator& aIterator,
@@ -40,18 +34,6 @@ class nsJXLDecoder final : public Decoder {
   explicit nsJXLDecoder(RasterImage* aImage);
 
   enum class DecoderState { Initial, HaveBasicInfo };
-
-  // jxl-rs output pixel format. Determines buffer stride, the conversion path
-  // in WritePixelRowsToPipe, and the SurfacePipe input format.
-  enum class PixelFormat {
-    Rgba8,       // u8 R,G,B,A (A=255 when no alpha channel)
-    Gray8,       // u8 G
-    GrayAlpha8,  // u8 G,A
-    Cmyk8,       // u8 C,M,Y,_ (4th byte unused); u8 K in mKBuffer
-    Rgba16f,     // f16 R,G,B,A, native-endian; HDR images when CMS was
-                 // requested (mTransform may still be null if transform
-                 // creation failed)
-  };
 
   enum class FrameOutputResult {
     BufferAllocated,
@@ -72,32 +54,11 @@ class nsJXLDecoder final : public Decoder {
   LexerResult ScanForFrameCount(SourceBufferIterator& aIterator,
                                 IResumable* aOnResume);
 
-  static PixelFormat DetectPixelFormat(JxlApiDecoder* aDecoder,
-                                       const JxlBasicInfo& aBasicInfo);
   FrameOutputResult BeginFrame();
-  void BuildCMSTransform();
   nsresult FinishFrame();
   void FlushPartialFrame();
-  bool WritePixelRowsToPipe();
 
   LexerResult DrainFrames();
-
-  size_t BytesPerPixel() const {
-    switch (mPixelFormat.value()) {
-      case PixelFormat::Rgba8:
-        return 4;
-      case PixelFormat::Gray8:
-        return 1;
-      case PixelFormat::GrayAlpha8:
-        return 2;
-      case PixelFormat::Cmyk8:
-        return 4;
-      case PixelFormat::Rgba16f:
-        return 8;
-    }
-    MOZ_ASSERT_UNREACHABLE("unhandled PixelFormat");
-    return 4;
-  }
 
   std::unique_ptr<JxlApiDecoder, JxlDecoderDeleter> mDecoder;
   std::unique_ptr<JxlApiDecoder, JxlDecoderDeleter> mScanner;
@@ -106,42 +67,10 @@ class nsJXLDecoder final : public Decoder {
 
   uint32_t mFrameIndex = 0;
 
-  // Field wrapper that asserts on read before first write and asserts on any
-  // write after the first.
-  template <typename T>
-  class WriteOnce {
-   public:
-    T value() const {
-      MOZ_ASSERT(mIsSet);
-      return mValue;
-    }
-    void set(T aVal) {
-      MOZ_ASSERT(!mIsSet);
-      mIsSet = true;
-      mValue = aVal;
-    }
-
-   private:
-    bool mIsSet = false;
-    T mValue{};
-  };
-
-  WriteOnce<PixelFormat> mPixelFormat;
-
-  // Per-row u8 output buffer for manual CMS paths (HDR, gray, CMYK).
-  Vector<uint8_t> mU8RowBuf;
-
-  // Full-frame decoded pixel buffer; allocated in BeginFrame, sized
-  // width * height * BytesPerPixel(). Passed to jxl-rs as the output buffer.
   Vector<uint8_t> mPixelBuffer;
-  Vector<uint8_t> mKBuffer;  // K (Black) channel, 1 byte/pixel, for CMYK images
   Maybe<SurfacePipe> mCurrentPipe;
 
   bool mIteratorComplete = false;
-
-#ifdef DEBUG
-  uint32_t mWritePixelRowsCount = 0;
-#endif
 };
 
 }  // namespace mozilla::image
