e4344e3d3c0197238a6ba176ce605ead5446bb90
Noah Peterson <NoahBPeterson1997@gmail.com>
2026-05-07 21:37:35 +0000

    Bug 1829401 - Part 1: Implements Element.requestPointerLock() to return Promise<undefined>; r=smaug,webidl
    Matching the spec at https://w3c.github.io/pointerlock/#requestPointerLock.
    
    Co-authored-by: Edgar Chen <echen@mozilla.com>
    
    Differential Revision: https://phabricator.services.mozilla.com/D299009

--- dom/base/PointerLockManager.h
+++ dom/base/PointerLockManager.h
@@ -6,6 +6,7 @@
 #define mozilla_PointerLockManager_h
 
 #include "mozilla/AlreadyAddRefed.h"
+#include "mozilla/RefPtr.h"
 #include "nsIWeakReferenceUtils.h"
 #include "nsThreadUtils.h"
 
@@ -18,11 +19,17 @@ class BrowserParent;
 enum class CallerType : uint32_t;
 class Document;
 class Element;
+class Promise;
 }  // namespace dom
 
 class PointerLockManager final {
  public:
-  static void RequestLock(dom::Element* aElement, dom::CallerType aCallerType);
+  // https://w3c.github.io/pointerlock/#dom-element-requestpointerlock
+  // |aPromise| is the Promise returned to script; it is resolved when the
+  // lock is acquired and rejected with a DOMException matching the spec
+  // when any check fails.
+  static void RequestLock(dom::Element* aElement, dom::CallerType aCallerType,
+                          dom::Promise* aPromise);
 
   MOZ_CAN_RUN_SCRIPT_BOUNDARY
   static void Unlock(const char* aReason, dom::Document* aDoc = nullptr);
@@ -52,13 +59,18 @@ class PointerLockManager final {
  private:
   class PointerLockRequest final : public Runnable {
    public:
-    PointerLockRequest(dom::Element* aElement, bool aUserInputOrChromeCaller);
+    PointerLockRequest(dom::Element* aElement, bool aUserInputOrChromeCaller,
+                       dom::Promise* aPromise);
     MOZ_CAN_RUN_SCRIPT_BOUNDARY NS_IMETHOD Run() final;
 
    private:
     nsWeakPtr mElement;
     nsWeakPtr mDocument;
     bool mUserInputOrChromeCaller;
+    // Strong reference: the script-side lifetime of the returned Promise is
+    // independent of the requested element's lifetime, so we keep it alive
+    // until we resolve or reject it.
+    RefPtr<dom::Promise> mPromise;
   };
 
   static void ChangePointerLockedElement(dom::Element* aElement,
