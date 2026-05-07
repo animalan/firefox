1ab110ab2f50efe826bd7559c4f0aa6d28a509ea
Daniel Holbert <dholbert@cs.stanford.edu>
2026-05-07 07:47:11 +0000

    Bug 2037761: Change RegisterStrongAsyncMemoryReporter to take already_AddRefed instead of a raw pointer, and update callsites. r=dom-core,smaug
    This is very similar to the previous patch in this series, but now for the
    version of this function with "Async" in the name.
    
    Differential Revision: https://phabricator.services.mozilla.com/D299044

--- xpcom/base/nsMemoryReporterManager.cpp
+++ xpcom/base/nsMemoryReporterManager.cpp
@@ -2876,9 +2876,8 @@ nsresult RegisterStrongMemoryReporter(
   return mgr->RegisterStrongReporter(reporter);
 }
 
-nsresult RegisterStrongAsyncMemoryReporter(nsIMemoryReporter* aReporter) {
-  // Hold a strong reference to the argument to make sure it gets released if
-  // we return early below.
+nsresult RegisterStrongAsyncMemoryReporter(
+    already_AddRefed<nsIMemoryReporter> aReporter) {
   nsCOMPtr<nsIMemoryReporter> reporter = aReporter;
   GET_MEMORY_REPORTER_MANAGER(mgr)
   return mgr->RegisterStrongAsyncReporter(reporter);
