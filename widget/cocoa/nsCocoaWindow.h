c2ca38e2e2b3da23471211f6742db5a6de8b232d
mr. m <mr.m@tuta.com>
2026-05-08 22:47:58 +0000

    Bug 1991893 - Start making use of native popovers on macos r=desktop-theme-reviewers,accessibility-platform-reviewers,mac-reviewers,emilio,layout-reviewers,omc-reviewers,aminomancer,bradwerth,eeejay,dao
    Differential Revision: https://phabricator.services.mozilla.com/D284084

--- widget/cocoa/nsCocoaWindow.h
+++ widget/cocoa/nsCocoaWindow.h
@@ -134,9 +134,14 @@ class TextInputHandler;
 
 @end
 
-@interface PopupWindow : BaseWindow {
+@interface PopupWindow : BaseWindow <NSPopoverDelegate> {
  @private
   BOOL mIsContextMenu;
+
+  // NSPopover support for native appearance
+  NSPopover* mPopover;
+  NSViewController* mPopoverViewController;
+  BOOL mUsePopover;
 }
 
 - (id)initWithContentRect:(NSRect)contentRect
@@ -146,6 +151,16 @@ class TextInputHandler;
 - (BOOL)isContextMenu;
 - (void)setIsContextMenu:(BOOL)flag;
 - (BOOL)canBecomeMainWindow;
+
+// NSPopover support
+- (void)setAllowPopover;
+- (BOOL)usePopover;
+- (void)showPopoverRelativeToRect:(NSRect)positioningRect
+                           ofView:(NSView*)positioningView
+                    preferredEdge:(NSRectEdge)preferredEdge
+                     hiddenAnchor:(BOOL)hiddenAnchor;
+- (void)closePopover;
+- (void)updatePopoverContent;
 
 @end
 
@@ -201,6 +216,10 @@ class nsCocoaWindow final : public nsIWidget {
 
  public:
   nsCocoaWindow();
+
+  // Check if this window should use NSPopover for popup/menu display
+  bool ShouldUseNSPopover() const;
+  bool ShouldShowAsNSPopover() const;
 
   [[nodiscard]] nsresult Create(nsIWidget* aParent, const DesktopIntRect& aRect,
                                 const InitData&) override;
