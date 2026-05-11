798ce755c4a427174cb0a14129f4b84afdc43ccb
stransky <stransky@redhat.com>
2026-05-11 11:32:59 +0000

    Bug 2000840 [Wayland] Fire emulated VSync events for unmapped surfaces r=emilio
    - Add explicit flag to VSync handler to mark emulated VSync callbacks.
    - Use timestamps for emulated callbacks.
    - Allow to run emulated VSync on unmapped surfaces - it's managed by VSync source now.
    - Remove emulated VSync from child layers of HDR rendering path, use HW only.
    
    Differential Revision: https://phabricator.services.mozilla.com/D298981

--- gfx/layers/NativeLayerWayland.cpp
+++ gfx/layers/NativeLayerWayland.cpp
@@ -517,6 +517,8 @@ bool NativeLayerRootWayland::CommitToScreen() {
 
   if (!mRootSurface->IsMapped()) {
     // TODO: Register frame callback to paint again? Are we hidden?
+    // TODO: Allow offscreen rendering to layers, attach them as subsurfaces
+    // on map
     LOG("NativeLayerRootWayland::CommitToScreen() root surface is not mapped");
     return false;
   }
@@ -600,9 +602,12 @@ bool NativeLayerRootWayland::CommitToScreen() {
   return true;
 }
 
-// Ready-to-paint signal from root or child surfaces. Route it to
+// Ready-to-paint signal (VSync) from child surfaces. Route it to
 // root WaylandSurface (owned by nsWindow) where it's used to fire VSync.
-void NativeLayerRootWayland::FrameCallbackHandler(uint32_t aTime) {
+void NativeLayerRootWayland::VSyncCallbackHandler(uint32_t aTime,
+                                                  bool aEmulated) {
+  MOZ_DIAGNOSTIC_ASSERT(!aEmulated,
+                        "VSyncCallbackHandler() is supposed to be HW only");
   {
     // Child layer wl_subsurface already requested next frame callback
     // and we need to commit to root surface too as we're in
@@ -612,15 +617,18 @@ void NativeLayerRootWayland::FrameCallbackHandler(uint32_t aTime) {
 
   if (aTime <= mLastFrameCallbackTime) {
     LOGVERBOSE(
-        "NativeLayerRootWayland::FrameCallbackHandler() ignoring redundant "
+        "NativeLayerRootWayland::VSyncCallbackHandler() ignoring redundant "
         "callback %d",
         aTime);
     return;
   }
   mLastFrameCallbackTime = aTime;
 
-  LOGVERBOSE("NativeLayerRootWayland::FrameCallbackHandler() time %d", aTime);
+  LOGVERBOSE(
+      "NativeLayerRootWayland::VSyncCallbackHandler() time %d emulated [%d]",
+      aTime, aEmulated);
   mRootSurface->VSyncCallbackHandler(nullptr, aTime,
+                                     /* aEmulated */ aEmulated,
                                      /* aRoutedFromChildSurface */ true);
 }
 
@@ -950,14 +958,16 @@ bool NativeLayerWayland::Map(WaylandSurfaceLock* aParentWaylandSurfaceLock) {
   //
   // aTime param is used to identify duplicate events.
   //
-  mSurface->SetVSyncCallbackLocked(
+  mSurface->SetVSyncCallbackHandlerLocked(
       surfaceLock,
-      [this, self = RefPtr{this}](wl_callback* aCallback,
-                                  uint32_t aTime) -> void {
-        LOGVERBOSE("NativeLayerWayland::FrameCallbackHandler() time %d", aTime);
-        mRootLayer->FrameCallbackHandler(aTime);
-      },
-      /* aEmulateFrameCallback */ true);
+      [this, self = RefPtr{this}](wl_callback* aCallback, uint32_t aTime,
+                                  bool aEmulated) -> void {
+        LOGVERBOSE(
+            "NativeLayerWayland::VSyncCallbackHandler() time %d emulated %d",
+            aTime, aEmulated);
+        MOZ_DIAGNOSTIC_ASSERT(!aEmulated);
+        mRootLayer->VSyncCallbackHandler(aTime, aEmulated);
+      });
 
   if (mIsHDR) {
     gfx::YUVColorSpace yuvColorSpace = gfx::YUVColorSpace::BT709;
