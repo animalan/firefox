d1ee4bd4a8feb40b092f8d273f8a4a05b4afb4a8
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2026-05-11 09:12:43 +0000

    Bug 2031468 - Disable ::-webkit-scrollbar on a per-domain blocklist. r=firefox-style-system-reviewers,layout-reviewers,emilio
    Some sites probe `@supports selector(::-webkit-scrollbar)` to detect
    WebKit/Blink and then ship styles that depend on real WebKit scrollbar
    behavior, which our partial `::-webkit-scrollbar` support does not match.
    Give us a way to disable the pseudo-element on specific domains while
    keeping it enabled everywhere else; on listed domains the pseudo is
    treated as unknown by the selector parser, so `@supports selector(...)`
    also reports it as unsupported.
    
    Differential Revision: https://phabricator.services.mozilla.com/D299295

--- dom/animation/KeyframeEffect.cpp
+++ dom/animation/KeyframeEffect.cpp
@@ -775,7 +775,7 @@ static const KeyframeEffectOptions& KeyframeEffectOptionsFromUnion(
 
 template <class OptionsType>
 static KeyframeEffectParams KeyframeEffectParamsFromUnion(
-    const OptionsType& aOptions, CallerType aCallerType, ErrorResult& aRv) {
+    const OptionsType& aOptions, Document* aDocument, ErrorResult& aRv) {
   KeyframeEffectParams result;
   if (aOptions.IsUnrestrictedDouble()) {
     return result;
@@ -792,8 +792,8 @@ static KeyframeEffectParams KeyframeEffectParamsFromUnion(
     return result;
   }
 
-  Maybe<PseudoStyleRequest> pseudoRequest =
-      PseudoStyleRequest::Parse(options.mPseudoElement);
+  Maybe<PseudoStyleRequest> pseudoRequest = PseudoStyleRequest::Parse(
+      options.mPseudoElement, aDocument->DefaultStyleAttrURLData());
   if (!pseudoRequest) {
     // Per the spec, we throw SyntaxError for syntactically invalid pseudos.
     aRv.ThrowSyntaxError(
@@ -835,7 +835,7 @@ already_AddRefed<KeyframeEffect> KeyframeEffect::ConstructKeyframeEffect(
   }
 
   KeyframeEffectParams effectOptions =
-      KeyframeEffectParamsFromUnion(aOptions, aGlobal.CallerType(), aRv);
+      KeyframeEffectParamsFromUnion(aOptions, doc, aRv);
   // An invalid Pseudo-element aborts all further steps.
   if (aRv.Failed()) {
     return nullptr;
@@ -1100,8 +1100,8 @@ void KeyframeEffect::SetPseudoElement(const nsAString& aPseudoElement,
 
   // Note: ParsePseudoELement() returns Some(NotPseudo) for the null string,
   // so we handle null case before this.
-  Maybe<PseudoStyleRequest> pseudoRequest =
-      PseudoStyleRequest::Parse(aPseudoElement);
+  Maybe<PseudoStyleRequest> pseudoRequest = PseudoStyleRequest::Parse(
+      aPseudoElement, mDocument->DefaultStyleAttrURLData());
   if (!pseudoRequest || pseudoRequest->IsNotPseudo()) {
     // Per the spec, we throw SyntaxError for syntactically invalid pseudos.
     aRv.ThrowSyntaxError(
