a39a5a9241155e63d7d529337b1f773b39cd9e22
Silaghi <asilaghi@mozilla.com>
2026-05-12 01:14:13 +0300

    Revert "Bug 2022156 - rust storage primary password support r=dimi,fluent-reviewers,bolsson,mtigley" for causing bc failures at browser_passwords_primary_password
    This reverts commit a7a8bc8fba75c38a77d9b7f079b8e49b5314c825.

--- browser/components/aboutlogins/tests/browser/browser_primaryPassword.js
+++ browser/components/aboutlogins/tests/browser/browser_primaryPassword.js
@@ -18,10 +18,6 @@ function waitForLoginCountToReach(browser, loginCount) {
 }
 
 add_setup(async function () {
-  // ensure the rust mirror is disabled (Rust has its own PrP dialog)
-  await SpecialPowers.pushPrefEnv({
-    set: [["signon.rustMirror.enabled", false]],
-  });
   await addLogin(TEST_LOGIN1);
 
   // head.js enables OS auth for all tests in this directory but since we
@@ -32,7 +28,6 @@ add_setup(async function () {
   registerCleanupFunction(async () => {
     await Services.logins.removeAllUserFacingLoginsAsync();
     LoginTestUtils.primaryPassword.disable();
-    await SpecialPowers.popPrefEnv();
   });
 });
 
