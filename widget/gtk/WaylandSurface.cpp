18caf7026a4a310fa9e76b5b7f9857c8158e4ea9
Martin Stransky <stransky@redhat.com>
2026-05-13 11:35:49 +0000

    Bug 2000840 [Wayland] Add an external handler to configure emulated VSync run r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D299077

--- widget/gtk/WaylandSurface.cpp
+++ widget/gtk/WaylandSurface.cpp
@@ -219,7 +219,8 @@ void WaylandSurface::SetVSyncCallbackLocked(
   // Request VSync emulation if it's requested and we can't get regular
   // Vsync (we're hidden).
   if (HasEmulatedVSyncCallbackLocked(aProofOfLock) &&
-      !mEmulatedVSyncCallbackTimerID && (!mBufferAttached || !mIsMapped)) {
+      !mEmulatedVSyncCallbackTimerID && mVSyncEmulateCheck &&
+      mVSyncEmulateCheck()) {
     LOGVERBOSE(
         "WaylandSurface::SetVSyncCallbackLocked() emulated, schedule "
         "next check");
@@ -316,6 +317,19 @@ void WaylandSurface::SetVSyncCallbackStateHandlerLocked(
   LOGVERBOSE("WaylandSurface::SetVSyncCallbackStateHandlerLocked()");
   MOZ_DIAGNOSTIC_ASSERT(&aProofOfLock == mSurfaceLock);
   mVSyncCallbackStateHandler = aVSyncCallbackStateHandler;
+}
+
+void WaylandSurface::SetVSyncEmulateCheckLocked(
+    const WaylandSurfaceLock& aProofOfLock,
+    const std::function<bool(void)>& aVSyncEmulateCheck, bool aForce) {
+  LOGVERBOSE("WaylandSurface::SetVSyncEmulateCheckLocked()");
+  MOZ_DIAGNOSTIC_ASSERT(&aProofOfLock == mSurfaceLock);
+  if (!mVSyncEmulateCheck || aForce) {
+    mVSyncEmulateCheck = aVSyncEmulateCheck;
+  }
+  if (!mVSyncEmulateCheck) {
+    MozClearHandleID(mEmulatedVSyncCallbackTimerID, g_source_remove);
+  }
 }
 
 bool WaylandSurface::CreateViewportLocked(
