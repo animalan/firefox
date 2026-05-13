12ac70c709c7eb53354d7b059ef0e981c43c1431
Serban Stanca <sstanca@mozilla.com>
2026-05-14 01:13:28 +0300

    Revert "Bug 2037772: Tighten up lifetime management of imgLoader::sMemReporter. r=tnikkel" for causing leaks.
    This reverts commit b504342c45878786fd6a4fd1ba035dd8e450987b.

--- image/imgLoader.cpp
+++ image/imgLoader.cpp
@@ -208,7 +208,6 @@ class imgMemoryReporter final : public nsIMemoryReporter {
   void UnregisterLoader(imgLoader* aLoader) {
     mKnownLoaders.RemoveElement(aLoader);
   }
-  void UnregisterAllLoaders() { mKnownLoaders.Clear(); }
 
  private:
   nsTArray<imgLoader*> mKnownLoaders;
@@ -1243,7 +1242,7 @@ void imgCacheExpirationTracker::NotifyExpired(imgCacheEntry* entry) {
 
 double imgLoader::sCacheTimeWeight;
 uint32_t imgLoader::sCacheMaxSize;
-StaticRefPtr<imgMemoryReporter> imgLoader::sMemReporter;
+imgMemoryReporter* imgLoader::sMemReporter;
 
 NS_IMPL_ISUPPORTS(imgLoader, imgILoader, nsIContentSniffer, imgICache,
                   nsISupportsWeakReference, nsIObserver)
@@ -1282,6 +1281,7 @@ imgLoader* imgLoader::PrivateBrowsingLoader() {
 imgLoader::imgLoader()
     : mUncachedImagesMutex("imgLoader::UncachedImages"),
       mRespectPrivacy(false) {
+  sMemReporter->AddRef();
   sMemReporter->RegisterLoader(this);
 }
 
@@ -1295,16 +1295,8 @@ imgLoader::~imgLoader() {
       req->ClearLoader();
     }
   }
-
-  // If sMemReporter is still reachable (i.e. if this is before shutdown),
-  // unregister ourselves from it, so that it doesn't retain a pointer to our
-  // about-to-be-freed memory. (Note that if sMemReporter became null out from
-  // under us, then that means its pointers-to-imgLoaders have all been cleared
-  // via UnregisterAllLoaders, even if there's another strong reference keeping
-  // the imgMemoryReporter object itself alive beyond this point.)
-  if (sMemReporter) {
-    sMemReporter->UnregisterLoader(this);
-  }
+  sMemReporter->UnregisterLoader(this);
+  sMemReporter->Release();
 }
 
 void imgLoader::VerifyCacheSizes() {
@@ -1330,7 +1322,7 @@ void imgLoader::GlobalInit() {
   int32_t cachesize = StaticPrefs::image_cache_size_AtStartup();
   sCacheMaxSize = cachesize > 0 ? cachesize : 0;
 
-  sMemReporter = MakeRefPtr<imgMemoryReporter>();
+  sMemReporter = new imgMemoryReporter();
   RegisterStrongAsyncMemoryReporter(do_AddRef(sMemReporter));
   RegisterImagesContentUsedUncompressedDistinguishedAmount(
       imgMemoryReporter::ImagesContentUsedUncompressedDistinguishedAmount);
@@ -1338,14 +1330,7 @@ void imgLoader::GlobalInit() {
 
 void imgLoader::ShutdownMemoryReporter() {
   UnregisterImagesContentUsedUncompressedDistinguishedAmount();
-  if (sMemReporter) {
-    UnregisterStrongMemoryReporter(sMemReporter);
-    // Ensure sMemReporter doesn't have any pointers back to any imgLoaders,
-    // since we're about to clear the reference that they would use to
-    // unregister themselves when they're destroyed:
-    sMemReporter->UnregisterAllLoaders();
-    sMemReporter = nullptr;
-  }
+  UnregisterStrongMemoryReporter(sMemReporter);
 }
 
 nsresult imgLoader::InitCache() {
