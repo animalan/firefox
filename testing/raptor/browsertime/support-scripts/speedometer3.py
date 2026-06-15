1dde7d69e3326e7a9d879123371f5df3d689b19a
Abhishek Nimalan <abhisheknimalan@gmail.com>
2026-06-14 21:16:58 -0400

    Bug 2030166 - Add transform and Autoland tasks for Speedometer 3 native profiling on macOS. r?#perftest
    These transforms configures Samply profiling and symbolication dependencies
    for Speedometer 3 macOS CI tasks when --native-profiling is used or
    when running on Autoland.
    
    Differential Revision: https://phabricator.services.mozilla.com/D306849

--- testing/raptor/browsertime/support-scripts/speedometer3.py
+++ testing/raptor/browsertime/support-scripts/speedometer3.py
@@ -27,6 +27,14 @@ class Speedometer3Support(BasePythonSupport):
                 f"{test.get('browsertime_args', '')} --browsertime.post_startup_delay=2000".strip()
             )
 
+        if args.samply_profile:
+            test["samply_profile"] = True
+            if args.browser_cycles is None:
+                test["browser_cycles"] = 20
+            test["browsertime_args"] = (
+                f"{test.get('browsertime_args', '')} --browsertime.post_startup_delay=2000".strip()
+            )
+
         if args.simpleperf:
             # Each test suite runs in its own browser cycle.
             # There's 20 test suites, so 20 cycles are needed.
@@ -150,7 +158,12 @@ class Speedometer3Support(BasePythonSupport):
         if self.platform == "Windows":
             suite["alertSeverity"] = "critical"
 
-        if test.get("simpleperf", False) or test.get("etw_profile", False):
+        native_profiling_flags = [
+            "simpleperf",
+            "etw_profile",
+            "samply_profile",
+        ]
+        if any(test.get(flag) for flag in native_profiling_flags):
             suite["shouldAlert"] = False
             for subtest in suite.get("subtests", []):
                 subtest["shouldAlert"] = False
